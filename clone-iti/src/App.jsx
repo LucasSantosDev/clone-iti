2a3ada8934a3c9ce86d76f4bdcbf272cac8319af
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-07-25 19:00:07 -0300

    sending header

---
+++ clone-iti/src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./Components/Header";
+import './global.css';
+
+export function App() {
+  return (
+   <div>
+      <Header />
+   </div>
+  )
+}
---
+++ clone-iti/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.css'
+import itauLogo from '../assets/itau-logo.svg'
+import { IoIosContrast } from 'react-icons/io'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.rightSide}>
+          <img src={itauLogo} alt="Logotipo do itaú"/>
+          <p>abra sua conta</p>
+        </div>
+
+        <button>
+          <IoIosContrast />
+        </button>
+      </div>
+    </header>
+  );
+}
