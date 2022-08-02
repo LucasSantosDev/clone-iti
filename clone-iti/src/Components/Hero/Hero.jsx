ca17437c8268a7434d05e72bff4dc75b11a04131
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-08-02 14:51:58 -0300

    repairing the responsivity and putting the prettier to autoformat

--- clone-iti/src/Components/Hero/Hero.jsx
+++ clone-iti/src/Components/Hero/Hero.jsx
@@ -14,8 +14,12 @@ export function Hero() {
               alt="Hero background"
             />
           </div>
+
           <div className={styles.heroText}>
-            <h1 className={styles.h1}>o banco digital do itaú</h1>
+            <h1 className={styles.h1}>
+              o banco digital <span className={styles.spanTitle}>grátis </span>
+              do itaú
+            </h1>
             <p className={styles.paragrafo}>
               a conta <strong>completa</strong> onde seu dinheiro{" "}
               <strong>rende mais que a poupança</strong>
@@ -23,9 +27,9 @@ export function Hero() {
           </div>
 
           <div className={styles.divMion}>
-            <img 
-              className={styles.pictureMion} 
-              src={pictureMion} 
+            <img
+              className={styles.pictureMion}
+              src={pictureMion}
               alt="image of presenter Marcus Mion showing the iti app on a cell phone"
             />
           </div>
